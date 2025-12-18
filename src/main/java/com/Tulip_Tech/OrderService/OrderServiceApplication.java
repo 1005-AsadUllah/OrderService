49323aaa469d35a3c23a1bf8387dd5837141f21e
Your New Name <aullah@tulip-tech.com>
2025-12-18 17:01:10 +0600

    fix: update application properties and YAML for environment variable support

--- src/main/java/com/Tulip_Tech/OrderService/OrderServiceApplication.java
+++ src/main/java/com/Tulip_Tech/OrderService/OrderServiceApplication.java
@@ -8,7 +8,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 //@EnableFeignClients
 public class OrderServiceApplication {
 
-    static void main(String[] args) {
+   public static void main(String[] args) {
 		SpringApplication.run(OrderServiceApplication.class, args);
 	}
 
